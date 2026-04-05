4c69dbd28ec8d48aa23e795297e444395fb6c9d4
jatin kumar <mrbansal157134@gmail.com>
2026-04-05 09:43:34 +0530

    changes in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kbg-game/',   // 🔥 THIS LINE FIXES YOUR ISSUE
+
   esbuild: {
     jsx: 'transform',
     jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
+    jsxFragment: 'React.Fragment'
   }
 })
